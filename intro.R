a6b651e34f353ca47e5a280d5cbe82528edd8174
Raveena <raveena.nair01@gmail.com>
2025-02-12 10:03:50 +0530

    Third Commit

--- intro.R
+++ intro.R
@@ -1,2 +1,90 @@
-x=19
-y=20
+##Practice operations
+2+2
+4+5
+1+88
+1-9
+8/2
+a=8
+#### Types of Operator: 1)Arithmetic Operators: + - / * () Require two operands
+
+(((10+2)-2)/5)^2
+
+a=3
+a
+b=9
+b
+a+b
+a*b
+a/b
+b/a
+a-b
+b^a
+4^2
+4^2/2^2
+
+log(100,10)
+log(100)
+#### Types of Operator: 2)Relational Operators: == > < >= <= != operations
+x=9
+y=5
+x==y
+x>y
+x<=y
+x!=y
+5==5
+a=(5==5)
+a
+
+F*50
+T*50
+
+5>2
+5!=7
+5>=3
+3<=9
+3==3 & 4>5
+3==3 | 4>5
+#### Types of Operator: 3)Logical Operators: & | Perform Boolean operations
+
+T&T
+T&F
+F&F
+T|T
+T|F
+F|F
+T&F|F
+## Data structures in R: Vectors
+
+###Constructing Vector in R
+V = c(0,9,19,15,25,625)
+V
+head(V)
+tail(V)
+V1=5:100
+V1
+head(V1)
+tail(V1)
+V2=4.8:9.6
+V2
+V3=seq(4,9, by =0.4)
+V3
+V4=seq(10,20, length=5)
+V4
+### Indexing of vectors
+V=seq(10,90, by=5)
+V
+V[2]
+V[6]
+V[1:5]
+V[6:10]
+V
+V[-2] #Deleting 2nd number
+V[-(2:5)]
+### Vector element sorting
+V = c(10,17,25,4,6,9,14)
+sort(V)
+sort(V, decreasing = T)
+c=c("red","yellow","blue","pink","black","lavender","orange")
+c
+sort(c)
+getwd()
